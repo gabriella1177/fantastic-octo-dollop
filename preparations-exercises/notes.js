867bdcfef417ba1c1aa4f6b4a7b491149800e983
Gabriella De Francesco <gabriella.defrancesco92@gmail.com>
2022-04-18 18:18:25 -0700

    completed q 5

---
+++ preparations-exercises/notes.js
@@ -0,0 +1,17 @@
+/* 
+Identify the Constructors for each of the 
+following methods and classify each method 
+as either a "Static" or a "Prototype" method:
+
+substring - String, prototype
+create - Object, static
+fromCharCode - String, static
+slice - Array, prototype
+toString - Object, Number, String, prototype
+
+It might be that more than one constructor
+ provides a method with a given name. 
+ Make sure you list them all in your answer. 
+ You can limit your search for methods to the 
+String, Object, Array, and Number constructors. 
+*/
